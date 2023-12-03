1606160b41d8c546e3e2a48996d15b25a81c6d98
TassioTenorio <121881174+TassioTenorio@users.noreply.github.com>
2023-12-02 22:11:34 -0300

    creating form of login and form of createaccount

--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,8 +1,15 @@
 # aqui vamos criar a estrutura do banco de dados
 
-from fakepinterest import database
+from fakepinterest import database, login_manager
 from datetime import datetime
-class Usuario(database.Model):
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_usuario(id_usuario):
+    return Usuario.query.get(int(id_usuario))
+
+class Usuario(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False)
     email = database.Column(database.String, nullable=False, unique=True)
